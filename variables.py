d124a4177d0c243dbace73d96913c30744527fba
Enoch Mgonga <enoch@localhost.localdomain>
2019-05-15 19:13:13 +0300

    i have written a simple script demonstrating variables and keywords

--- variables.py
+++ variables.py
@@ -1,3 +1,19 @@
 '''
 Write a simple script to demonstrate your understanding of Variable,identifier and keywords
 '''
+message = 'I love python' # A string assignment
+pi = 3.14 # A float assignment
+n = 17 # an integer assignment
+17 = n # invalid assignment
+
+76name = 'John Doe' # This is an ivalid syntax
+more@ = 'python' # this is an ivalid syntax
+True = 'Yes' # True is a reversed keyword
+fname lname = 'Michael Jordan' # invalid syntax
+
+'''
+There is no need to memorize all the reserved keywords because in most editors they 
+are highlighted
+''' 
+
+
