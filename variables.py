47599f02632abcae87f91bf2f28e6737fd9b6897
enoch mgonga <emelchad@gmail.com>
2019-05-15 19:54:47 +0300

    Tried to output the variables with a print statement

--- variables.py
+++ variables.py
@@ -6,11 +6,19 @@ pi = 3.14 # A float assignment
 n = 17 # an integer assignment
 17 = n # invalid assignment
 
+print(message)
+print(n)
+print(17)
+
 76name = 'John Doe' # This is an ivalid syntax
 more@ = 'python' # this is an ivalid syntax
 True = 'Yes' # invalid syntax as True is a reversed keyword
 fname lname = 'Michael Jordan' # invalid syntax
 
+print(76name)
+print(more@)
+print(True)
+print(fname lname)
 '''
 There is no need to memorize all the reserved keywords because in most editors they 
 are highlighted
