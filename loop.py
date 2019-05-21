9bc9553b9b13d04b3e8ec8c959dbb793327d043e
Enock Mgonga <40548451+Enoch-27@users.noreply.github.com>
2019-05-22 00:53:19 +0300

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,3 +1,20 @@
 '''
 Write a simple script that demonstrate your understanding of loops in python   
 '''
+def countdown(n):
+    while n > 0:
+        print(n)
+        n = n - 1
+    print('Blastoff!\n\n')
+
+
+
+
+def print_letters(name):
+    for letter in name:
+        print(letter)
+
+
+countdown(10)
+name = "Enock"
+print_letters(name)
