dd5d2fcef97ad151ef16dbb2cccc0224a6cd0367
Enock Mgonga <40548451+Enoch-27@users.noreply.github.com>
2019-05-22 01:16:19 +0300

    written a script to demonstrate functions

--- functions.py
+++ functions.py
@@ -2,3 +2,13 @@
 Write a simple script that demonstrate your understanding of functions in python 
 '''
 
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+
+print(fibonacci(8))
