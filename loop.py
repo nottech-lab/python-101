36391fcec02bbfad44beb567bdfb40ab978086c1
Enock Mgonga <40548451+Enoch-27@users.noreply.github.com>
2019-05-22 11:06:09 +0300

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,10 +1,6 @@
 '''
 Write a simple script that demonstrate your understanding of loops in python   
 '''
-<<<<<<< HEAD
-
-=======
->>>>>>> 3a24ca06b03f1a38019bdaac5a3aabeb68e1e150
 def countdown(n):
     while n > 0:
         print(n)
@@ -21,8 +17,5 @@ def print_letters(name):
 
 countdown(10)
 name = "Enock"
-<<<<<<< HEAD
+
 print_letters(name)
-=======
-print_letters(name)
->>>>>>> 3a24ca06b03f1a38019bdaac5a3aabeb68e1e150
