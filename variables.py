2419064a796eb2e973e66eab3e1570ec6d33e26a
yeuni gilbert <yeunidev@gmail.com>
2019-05-27 22:40:24 +0300

    My first ever Commit Message about variables

--- variables.py
+++ variables.py
@@ -1,3 +1,19 @@
-'''
-Write a simple script to demonstrate your understanding of Variable,identifier and keywords
-'''
+#declaring and assigning a value to a variable
+geek_website = "hackathon.com"
+
+
+#assigning a new variable to Geek website
+geek_website = "nottechlab.com"
+
+print(geek_website)
+
+
+#assigning multiple values to multiple variables
+
+myName , myAge , myAddress = "Yeuni", "27 yrs Old" ,"Arusha"
+
+print (myName)
+print (myAge)
+print (myAddress)
+
+
