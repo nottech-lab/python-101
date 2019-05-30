43ba76e2f5f24a5edaeb87871448a55137b600eb
Erastus <erapukka@gmail.com>
2019-05-30 08:51:54 -0700

    i have showed what i have understood on variables by introducing my self

--- variables.py
+++ variables.py
@@ -1,3 +1,9 @@
 '''
 Write a simple script to demonstrate your understanding of Variable,identifier and keywords
 '''
+name = 'Erastus'
+age = 26 
+marital_status = 'single' 
+print('My name is Erastus I am 26 years old')
+print('My name is',name, 'i am', age,'years old')
+
