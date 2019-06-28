2ae9293ceca37df6cf9eb67c0f9c5e0269a3fd95
Enoch Mgonga <enoch@localhost.localdomain>
2019-06-28 03:00:18 +0300

    written about dictionaries

--- dictionary.py
+++ dictionary.py
@@ -1,3 +1,19 @@
 '''
-Write a simple script to demonstrate your understanding of dictionaries as data types 
+Write a simple script to demonstrate your understanding of dictionaries as data types
 '''
+def word_frequencies(words_list):
+    dictionary = {}
+
+    for word in words_list:
+        if word in dictionary:
+            dictionary[word] += 1
+        else:
+            dictionary[word] = 1
+
+    return dictionary
+
+words_list = ['i', 'messed', 'up', 'tonight', 'i', 'lost', 'another',
+                'fight', 'i', 'will', 'still', 'messup', 'now', 'to',
+                'see', 'what', 'is', 'next']
+
+print(word_frequencies(words_list))
