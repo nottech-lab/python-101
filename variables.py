6829156eec85745bc72f2be5b7efc5f68751515e
root <root@localhost.localdomain>
2019-08-10 20:14:34 +0300

    variable task

--- variables.py
+++ variables.py
@@ -1,3 +1,13 @@
 '''
 Write a simple script to demonstrate your understanding of Variable,identifier and keywords
 '''
+#In python variables are not declared,They are automatically created when given values
+
+
+bus_name = "DarExpress"           #declaring a string with double quotation          
+passengers_name = 'Josep Karage'  #declaring a string with a single quotation
+ticket_id = 765295                #declaring an integer
+seat_no = 45                      #declaring an integer also
+arrival_time = 06.45              #declaring a float
+
+print(seat_no)                    #displaying a variable seat_noS     
