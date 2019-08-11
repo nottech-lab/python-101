8981dd4c641578536019d9d3b087303cdfac1465
daudi mwanja <daudimwanja2014@gmail.com>
2019-08-11 11:36:07 -0700

    modified file of tuples

--- tuple.py
+++ tuple.py
@@ -1,3 +1,28 @@
 '''
 Write a simple script to demonstrate your understanding of tuples as data types 
 '''
+
+    # TUPLES.
+    
+# Lists are immutable means has the fixed value which can not be altered.
+
+# A tuple consists of a number of values separated by commas, for instance:
+
+tup = (2, 5, "name", 8, 10)
+
+print(tup) # the output is always in paranthesis.
+
+a, b, c, d, e = tup # assign in a reverse
+
+print(tup) # it gives the same values.
+
+print(tup[2]) # print the third value in a tuple
+
+print(tup[1:]) # it gives an output of tuple with second to last item.
+
+tup1 = (12, 22, 15)
+
+print(max(tup1)) # it gives the maximum value in the tuple.
+
+print(min(tup1)) # it gives the minimum value in the tuple.
+
