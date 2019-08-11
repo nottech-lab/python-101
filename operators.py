b92da750a852230ddfc61807b8fb1978ab5f5692
daudi mwanja <daudimwanja2014@gmail.com>
2019-08-11 11:51:33 -0700

    modified file of operators

--- operators.py
+++ operators.py
@@ -1,4 +1,82 @@
 '''
 Write a simple script that demonstrate your understanding of Operators,operands and operator precedence
 '''
+# BASIC OPERATORS.
+
+# NUMERIC OPERATORS.
+
+x = 5
+y = 6.0
+
+print(x+y) # additon
+
+print(x*y) # multiplication
+
+print(x**y) # power
+
+print(20%6) # remainder
+
+print(y//x) # floor division
+
+ # LOGIC OPERATORS.
+
+a, b, c = 10, 20, 30
+
+print(c)
+
+# == (equal to)) : Compares if the objects are equal
+
+if a == b:
+    print('a is equal to b')
+
+else:
+    print('a is not equal to b')
+    
+# != (not equal to): : Compares if the objects are not equal
+
+m, n = 3, 2
+
+if m != n:
+    print(' m is not equal to n')
+
+# not : (boolean NOT)
+
+
+# and (boolean AND)
+
+c = a % b
+
+print(c)
+
+# or (boolean OR)
+
+c = a | b
+
+print(c)
+# > (greater than): Returns whether x is greater than y
+
+x, y = 5, 3
+
+if x > y:
+    print('x is greater than y')
+# < (less than): Returns whether x is less than y.
+
+if x < y:
+    print('x is less than y')
+    
+# <= (less than or equal to)
+
+if x <= y:
+    print('x is equal to y')
+else:
+    print('x is not equal to y')
+        
+# >= (greater than or equal to)
+
+if x >= y:
+    print('x is equal to y')
+else:
+    print('x is not equal to y')
+        
+
 
