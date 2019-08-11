5d1752cc1ccd8ff5bbd585e8ff687b0f7e6ef728
root <root@localhost.localdomain>
2019-08-11 17:17:09 +0300

    initial operator commit

--- operators.py
+++ operators.py
@@ -1,4 +1,22 @@
 '''
 Write a simple script that demonstrate your understanding of Operators,operands and operator precedence
 '''
+#Operators are used to perform operations on variables and values(Mathematical and logical operations)
+
+single_watch_cost = 22000
+available_watches = 20
+parking_cost = 1000
+available_money= 50000
+
+
+#1.Arithmetic Operations
+total_cost = single_watch_cost + parking_cost                        #addition
+remaining  = available_money - total_cost                            #substraction
+parking_slots = available_money/parking_cost                         #division
+total_watch_cost = single_watch_cost * available_watches             #multiplication
+remainder = available_money % total_cost                             #modulus
+
+
+
+
 
