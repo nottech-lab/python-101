071d3cbc9aff5a60e2e53701efcbc0353add33c5
daudi mwanja <daudimwanja2014@gmail.com>
2019-08-11 12:36:21 -0700

    modified files of numbers

---
+++ numbers.py
@@ -0,0 +1,33 @@
+'''
+write exampled codes to demonstrate your understanding of numbers.
+
+'''
+    # NUMBERS.
+    
+# python numbers are created by the standard python method.
+
+# Number can either be integer, long, float, complex.
+
+msg = 'Good evening'
+
+num = 99
+
+pi = 3.14159
+
+print(type(msg)) # returns a string.
+
+print(type(num)) # returns an integer.
+
+print(type(pi)) # returns a float.
+
+a = 3.45
+print(int(a)) # convert to integer
+b = 3
+print(float(b)) # convert to float
+
+d = 2
+print(complex(d)) # convert to complex number with real part d and imaginary part zero.
+
+e = 5
+print(complex(e,d)) # convert to complex number with real part e and imaginary part d.
+
