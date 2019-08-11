e6dd4a2251730fff8f0e102a2161afa633ad4984
daudi mwanja <daudimwanja2014@gmail.com>
2019-08-11 11:39:38 -0700

    modified file of string

--- string.py
+++ string.py
@@ -1,3 +1,31 @@
 '''
 Write a simple script to demonstrate your understanding of strings 
 '''
+
+    # STRINGS.
+    
+# Can be declared either by single or double quotes.
+
+my_str="Daudi Emanuel" # creating a string
+
+print(my_str)    # print the value in the string.
+
+print(len(my_str)) # print the length of the string including spaces.
+
+print(my_str[0]) # print the first character in a string.
+
+print(my_str[1:]) # print the second to the last character in my string.
+
+print(my_str + " Mwanja ") # print the concatination of my string with another string.
+
+print(my_str.lower()) #  converts the whole string into lower case.
+
+print(my_str.upper()) # converts the whole string into upper case.
+
+print(my_str.islower()) # returns true if the string is in lower case.
+
+print(my_str.isupper())  # returns true if the string is in lower case.
+
+print(my_str.capitalize()) # change the first letter into upper case.
+
+print(my_str.isalpha()) # check if the sring is alphanumeric or not.
