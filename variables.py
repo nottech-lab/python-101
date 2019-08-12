e5e139338f955d654613daad512786f3deb15f26
Mageta Sukulu <magetasukulu@gmail.com>
2019-08-12 19:20:04 +0300

    Cleared everything on this script for case of learning

--- variables.py
+++ variables.py
@@ -1,20 +1,3 @@
 '''
 Write a simple script to demonstrate your understanding of Variable,identifier and keywords
 '''
-
-# The following script allows a user fill His or Her own required infomations and stores them
-
-print("Please provide the required information")
-# Takes the user inputs and stores them in the given variables
-fullName = input("Full name: ")
-dateOfBirth = input("Date of birth: ")
-gender = input("Sex: ")
-print("\n")
-
-print("Please make sure the information you provided are correct")
-# Prints out the received and stored user information
-print("Full name: " + fullName)
-print("Date of birth: " + dateOfBirth)
-print("Sex: " + gender + "\n")
-
-userFinalResponse = input("Type yes to submit and no to re-enter the required information: ")
