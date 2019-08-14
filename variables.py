c8ba8a749ec58df93145a1c025e6bca6072acecd
fay-esp96 <faithtunzo@gmail.com>
2019-08-14 23:00:11 +0300

    My first commit on variables and keywords

--- variables.py
+++ variables.py
@@ -1,3 +1,10 @@
 '''
 Write a simple script to demonstrate your understanding of Variable,identifier and keywords
 '''
+integerNum = 10   #Since an Identifier can be a name of a variable then "num" is both an identifier and a variable
+if (integerNum != 15) :
+    print ('The Number is not equal to 15')#In this code "print" and "if" are both keywords.
+
+num1, num2 ,num3 = 1, 2, 90
+
+print (num1, num2, num3) #num1,num2 and num3 are variables
