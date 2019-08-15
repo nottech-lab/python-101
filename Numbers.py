71e9cb5d57cec67c436ea9722340ec5208a2d82f
Sajj96 <captainsajjad4@gmail.com>
2019-08-15 12:00:46 +0300

    Added new file of Numbers

---
+++ Numbers.py
@@ -0,0 +1,12 @@
+import math            #Importing math module
+import random          #Imports random module
+
+var = 10               #Integer number 
+_var_ = 0.123          #Float number
+
+print(math.pi)         #Prints PI value 
+print(math.cos(30))    #Print the cosine of 30
+print(abs(-20))        #Print absolute value of -20
+print(math.pow(2,3))   #Print the power of 2 to 3
+print(random.random()) #Prints random numbers
+print(math.sqrt(100))  #Print square root of 100
