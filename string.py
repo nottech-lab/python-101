845cdcb60b90ba437a9cafdb946c164467e80a36
root <root@localhost.localdomain>
2019-08-15 13:25:31 +0300

    string commit

--- string.py
+++ string.py
@@ -3,23 +3,34 @@ Write a simple script to demonstrate your understanding of strings
 ''' 
 
 motd = "Take it from me,being alive is lucky"
-print(motd[1])                                     #returns second charecter, a
+print(motd[1])                                     #returns second charecter, 
+                                                    "a"
+print(motd[2:5])                                   #returns 3rd char to 4th charecter,
+                                                    "ke" 
 
-print(motd[2:5])                                   #returns 3rd char to 4th charecter,ke 
+motd_2= "   Take it from me,being alive is lucky   "
+print(motd_2.strip())                              #removes space at the begining and end of our str
+                                                    "Take it from me,being alive is lucky"
 
-print(motd.strip())                                #returns "You say lucky is on your side!??",removes space at the begining and end of our str
-
-print(len(motd))                                   #returns the length of the str
+print(len(motd))                                   #returns the length of the str,
+                                                    36
 
 print(motd.lower())                                #returns the str in lower cases
+                                                  "take it from me,being alive is lucky"
 
 print(motd.upper())                                #returns the str in upper cases
+                                                   "TAKE IT FROM ME,BEING ALIVE IS LUCKY"
+
 
 print(motd.replace("me", "Karage"))                #returns a str where me is replaced by Karage
+                                                   "Take it from Karage,being alive is lucky"
 
-print(motd.split(","))                             # returns a list of substrings separated by ,
+print(motd.split(","))                             #returns a list of substrings separated by ,
+                                                    ['Take it from me', 'being alive is lucky']
 
 
 print(motd.capitalize())                           #Capitalizes the first character and lowercases all others
+                                                     "Take it from me,being alive is lucky"
 
-print(motd.swapcase())                             ##Swaps case of each character
+print(motd.swapcase())                             #Swaps case of each character
+                                                    "tAKE IT FROM ME,BEING ALIVE IS LUCKY"
