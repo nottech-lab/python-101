b710295f7167eff8a9ee4749949540440d2c94c1
Tatu Kassim <tatukassim66@gmail.com>
2019-08-15 10:43:10 -0700

    Initial Commit

--- variables.py
+++ variables.py
@@ -1,3 +1,11 @@
 '''
 Write a simple script to demonstrate your understanding of Variable,identifier and keywords
 '''
+#here is the demostration of interger, string and float variables with print keyword
+interger = 360
+string = "personal information"
+float = 3.14
+
+print (interger)
+print ("string")
+print (float)
