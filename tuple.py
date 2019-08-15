1a53916eac5d8e0fa12de97499453b81d85992ac
Sajjad A Khalid <44890673+Sajj96@users.noreply.github.com>
2019-08-15 12:37:18 +0300

    Modified file

--- tuple.py
+++ tuple.py
@@ -9,7 +9,7 @@ print(tuple1[2:4])  #Print list starting from 3rd to 4th
 print(len(tuple1))  #Print length of the tuple1
 
 tuple2 = (12, 23, 45)
-print(max(tuple2))  #Print maximum value in tuple2
+print(max(tuple2))  #Print maximum value in tuple2.
 
 x = 23
 if x in tuple2:
