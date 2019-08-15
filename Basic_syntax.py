5a373e53af5363a57915672e9a8fff2a4576161c
Sajj96 <captainsajjad4@gmail.com>
2019-08-15 11:59:34 +0300

    Added new file of Basic Syntax

---
+++ Basic_syntax.py
@@ -0,0 +1,17 @@
+import sys
+
+print('Hello Nottech Lab')    #Prints out Hello Nottech Lab
+
+x = "I am Sajjad"             #Declaring variable and assigning value
+
+sys.stdout.write(x)     #Prints the value stored in x
+
+X  = "Am another value"       #The identifier X not equal to x
+
+#for = 10                      #Gives error because for is reserved keyword and can not be used as variable
+
+if True:                      #Basic selection syntax(Line and Identation)
+    print('Yes am in')
+else:
+    print('Not in')    
+
