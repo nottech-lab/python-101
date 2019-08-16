161257582dd24628532ab27d109b03ce37c4098b
fay-esp96 <faithtunzo@gmail.com>
2019-08-16 23:43:47 +0300

    A commit on Operators

--- operators.py
+++ operators.py
@@ -2,3 +2,24 @@
 Write a simple script that demonstrate your understanding of Operators,operands and operator precedence
 '''
 
+
+#"=" assigns "I love Python" to variable "phrase"
+phrase = "I love Python"
+
+print(phrase)
+
+#"%" is an arithmetic operator showing the remainder whereby "12" and "8" are operands
+
+remainder= 12 % 8
+print(remainder)
+
+#"==" is a comparison operator which returns  TRUE if the two arguments are the same else FALSE
+
+num1, num2 = 4,8
+print(num1 == num2)
+
+#the operator with the highest precedence starts and if more than one operator of the same precedence order are present then evaluation starts from left to right
+
+equation = 2 - 8 + 2 * 2
+print(equation)
+
