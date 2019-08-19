0cb9d345557c6f50de89b9f5fcdd1dff248ad068
fay-esp96 <faithtunzo@gmail.com>
2019-08-19 20:11:15 +0300

    A commit on Tuples

--- tuple.py
+++ tuple.py
@@ -1,3 +1,17 @@
 '''
 Write a simple script to demonstrate your understanding of tuples as data types 
 '''
+
+
+animals = ('cow', 'goat', 'rabbit', 'ants', 'fish', 'lamb')
+position = (1, 2, 3, 5, 8)
+
+print(animals[0],' and ',animals[2], ' are in the same phylum')  #prints items present in index 0 and 2 in the tuple present in variable 'animals'
+
+combination = animals + position     #creates new tuple
+
+print(combination)
+
+del(animals[0])
+
+print(animals)   #this will produce an error since a tuple cannot be updated
