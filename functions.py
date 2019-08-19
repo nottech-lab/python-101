188b05ca09aab64323a5c511987d6c2cc35da99c
fay-esp96 <faithtunzo@gmail.com>
2019-08-19 20:22:25 +0300

    A commit on Functions

--- functions.py
+++ functions.py
@@ -2,3 +2,22 @@
 Write a simple script that demonstrate your understanding of functions in python 
 '''
 
+
+#non-void funtion
+def product(number1,number2):
+    multiplication = number1 * number2
+    print(multiplication)
+    return
+
+
+intNum = 8
+intNum1 = 10
+
+if((intNum < 10)and (intNum1 == 10)):
+    product(intNum,intNum1)
+
+#void function
+def nothing():
+    print('This is void')
+
+nothing()
