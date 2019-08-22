b06c3008663006b41fcebdd3b1d30cd70db85023
root <root@localhost.localdomain>
2019-08-22 12:04:08 +0300

    dictionary commit

--- dictionary.py
+++ dictionary.py
@@ -1,3 +1,46 @@
 '''
 Write a simple script to demonstrate your understanding of dictionaries as data types 
 '''
+
+#A dictionary is a collection which is unordered, changeable and indexed
+
+
+#Creating a dictionary and printing it
+TeamCaptains=	{
+  "Liverpool": "Henderson",
+  "Manchester city": "Silva",
+  "Arsenal": "Shaka",
+  "Tottenham": "Loris",
+  "Real madrid": "Ramos",
+}
+print(TeamCaptains)
+
+#Accessing items
+print(TeamCaptains["Real madrid"])
+
+#Change values
+TeamCaptains["Liverpool"] ="Jkarage"
+print(TeamCaptains)
+
+#Looping in a dictionary
+for k,y in TeamCaptains.items():
+    print(k,y)
+
+#Check if a key exists
+if "Manchester united" in TeamCaptains:
+    print("A great team")
+else:
+    print("Looser")
+
+#Adding items
+TeamCaptains["Simba"] ="Bocco"
+
+#Remove an item
+TeamCaptains.pop("Liverpool")
+
+#Emptying the dictionary
+TeamCaptains.clear()
+
+
+#deleting the dictionary
+del(TeamCaptains)
