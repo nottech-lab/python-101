f862ac748ce84f728643c13c6886bb97b5642d95
fay-esp96 <faithtunzo@gmail.com>
2019-08-22 17:30:22 +0300

    A commit on sets

--- sets.py
+++ sets.py
@@ -1,3 +1,26 @@
 '''
 Write a simple script to demonstrate your understanding of sets as data types 
 '''
+
+fruits = {'apple', 'orange', 'banana', 'grapes'}
+
+#accessing sets
+for fruit in fruits:
+    print(fruit)
+
+print("")
+print("")
+print('Is Rabbit in the set of animals? ','rabbit' in fruits)
+
+#updating the set
+
+fruits.add('guava')
+print('Is Guava in the set of fruits? ','Guava' in fruits)
+
+fruits.update(['Cucumber', 'Strawberry'])
+for fruit in fruits:
+    print(fruit)
+
+#removing items
+fruits.remove('Orange')
+print('Is apple in the set of fruits? ','oranga' in fruits)
