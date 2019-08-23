7d979645593879620ac741da35bd3f485837fa20
Shangweli <shangwelikituma93@gmail.com>
2019-08-23 15:22:06 +0300

    A commit on list

--- list.py
+++ list.py
@@ -1,3 +1,15 @@
 '''
 Write a simple script to demonstrate your understanding of lists 
 '''
+# Assign the list
+lst = [8, -13, 10, 14, -1]
+# Make the first element 5
+lst[0] = 5
+# Print the second element
+print(lst[1])
+# Make the last element 12
+lst[4] = 12
+# Print a list variable
+print(lst)
+# Print second element of literal list
+print([10, 20, 30][1])
