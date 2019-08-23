a02938c1c595f56400c31446a29716b164f68efb
samsontz <franksam018@gmail.com>
2019-08-23 10:02:36 +0300

    A commit on conditional statments

--- conditional_statements.py
+++ conditional_statements.py
@@ -2,4 +2,12 @@
 Write a simple script that demonstrate your understanding of conditional statements 
 Simple If , if else , elif  
 '''
-
+#checking if the number is even or odd
+number = 6
+check = 2
+#ccheckin if the number is even
+if (number % check == 0):
+  print("The number is Even")
+#printing if the number is odd
+else: 
+  print("The number is not Odd")
