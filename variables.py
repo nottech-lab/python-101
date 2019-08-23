63723a00d50f0039e7e299689b9b1c85ba1a0151
samsontz <franksam018@gmail.com>
2019-08-23 09:51:12 +0300

    A commit on variables

--- variables.py
+++ variables.py
@@ -1,3 +1,10 @@
 '''
 Write a simple script to demonstrate your understanding of Variable,identifier and keywords
 '''
+#creating variables
+variable1 = 20
+variable2 =30
+#finding summation of variables
+summation = variable1 + variable2
+#printing the sum
+print("the sum of 20 and 30 is ",summation)
