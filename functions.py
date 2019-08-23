bf7673115a5eefc615e273b11bbcb7f078883f41
Shangweli <shangwelikituma93@gmail.com>
2019-08-23 15:19:21 +0300

    A commit on functions

--- functions.py
+++ functions.py
@@ -2,3 +2,12 @@
 Write a simple script that demonstrate your understanding of functions in python 
 '''
 
+
+# Function definition is here
+def printme( str ):
+    "This prints a passed string into this function"
+    print (str);
+    return;
+# Now you can call printme function
+printme("Printing the function for the first time!");
+printme("Printing the function for the second time");
