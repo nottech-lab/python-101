63502bbaf86107f080393eaf62b50d5c13ac9287
Shangweli <shangwelikituma93@gmail.com>
2019-08-23 15:24:49 +0300

    A commit on string

--- string.py
+++ string.py
@@ -1,3 +1,7 @@
 '''
 Write a simple script to demonstrate your understanding of strings 
 '''
+#declaring string
+message = "Hello word!"
+#printing message on the screen
+print("The message is ",message)
