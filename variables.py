25ff259bdb8c39dee3095fab494b0cf68f977550
MagimbiJr <charleatanasi@gmail.com>
2019-09-17 10:52:23 +0300

    Initial variables commit

--- variables.py
+++ variables.py
@@ -1,3 +1,11 @@
 '''
 Write a simple script to demonstrate your understanding of Variable,identifier and keywords
 '''
+
+name = 'Atanas Charle'
+age = 26
+tribe = 'Ngoni'
+
+print name
+print age
+print tribe
