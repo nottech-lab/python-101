203addd8f58bc1c43cc8d2c1e94115110d87a3ed
MagimbiJr <charleatanasi@gmail.com>
2019-09-17 11:20:50 +0300

    Initial Operators commit

--- operators.py
+++ operators.py
@@ -2,3 +2,17 @@
 Write a simple script that demonstrate your understanding of Operators,operands and operator precedence
 '''
 
+a = 20
+b = 30
+c = 2
+
+print a + b
+print b - a
+print b * c
+print b / c
+print a == c
+print a != c
+print a % c
+print a < b
+print b ** c
+
