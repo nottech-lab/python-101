71ecd44f079cbfb7c0cb98c0bb53c0ddcb956fc6
MagimbiJr <charleatanasi@gmail.com>
2019-09-19 13:56:39 +0300

    python dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,3 +1,19 @@
 '''
 Write a simple script to demonstrate your understanding of dictionaries as data types 
 '''
+
+dict = {'name': 'Tana', 'age': 26, 'class': 'First'}
+
+
+
+print (dict)
+print str(dict)
+
+print ('The length of dictionioray is'), len(dict)
+print ('Type of data is '), type(dict)
+
+dict['age'] = 30
+dict['region'] = 'Dar es salaam'
+
+print (dict)
+print (dict['name']), (dict['age']), (dict['region'])
