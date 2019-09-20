63589fb918706676dd9839d9bcb82dd5fcc5520c
MagimbiJr <charleatanasi@gmail.com>
2019-09-21 00:48:32 +0300

    Python sets

--- sets.py
+++ sets.py
@@ -1,3 +1,33 @@
 '''
 Write a simple script to demonstrate your understanding of sets as data types 
 '''
+
+thisset = {"Mangos", "Oranges", "Bananas"}
+for x in thisset:
+    print (x)
+
+print ('Mangos' in thisset)
+
+print (len(thisset))
+
+thisset.add('Pinapples')
+
+for x in thisset:
+    print(x)
+
+thisset.update(['Apples', 'Tangerine'])
+
+print (thisset)
+
+print (len(thisset))
+
+thisset.remove('Apples')
+thisset.remove('Mangos')
+
+print (thisset)
+
+thatset = {'Maize', 'Wheat', 'Beans'}
+
+bothsets = thisset.union(thatset)
+
+print (bothsets)
