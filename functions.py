a634ec2003fe504ca4358e5822302552938367da
daudi mwanja <daudimwanja2014@gmail.com>
2019-09-20 05:14:44 -0700

    Python functions

--- functions.py
+++ functions.py
@@ -2,3 +2,17 @@
 Write a simple script that demonstrate your understanding of functions in python 
 '''
 
+def my_name(name): # function definition.
+
+    print("My name is: " + name) # output. 
+    
+my_name("daudi mwanja") # function calling. 
+
+# function with return statement.
+
+def cube(number): # function defintion.
+    
+    return number*number*number # return cubic of the number.
+
+print(cube(3)) # show output to screen.
+
