e5f51215be43d7ca8510be6acf7e53c855f577a9
daudi mwanja <daudimwanja2014@gmail.com>
2019-09-20 04:55:08 -0700

    variables

--- variables.py
+++ variables.py
@@ -1,3 +1,20 @@
 '''
 Write a simple script to demonstrate your understanding of Variable,identifier and keywords
 '''
+# variables.
+string= "daudi mwanja" # assign value to variable string.
+
+int_num =10 # assign value to variable int_num.
+
+float_num=10.452 # assign value to variable float_num.
+
+boolean = True # assign value to variable boolean.
+
+print (string) # output string 'daudi mwanja' to screen.
+
+print (int_num) # output integer '10' to screen.
+
+print(float_num) # output float '10.452' to screen. 
+
+print(boolean) # output boolean 'True' to screen.
+
