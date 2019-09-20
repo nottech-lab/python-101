dcddfc0d29d7d1eecb8c18cac1b7aeb5bac49538
daudi mwanja <daudimwanja2014@gmail.com>
2019-09-20 11:26:47 -0700

    numbers

---
+++ numbers.py
@@ -0,0 +1,9 @@
+x = 5
+
+y = 10.234
+
+print(type(float(x))) # return type of number to be integer.
+
+print(type(int((y)))) # return type of number to be float.
+
+print(type(complex(y))) # return the type of number to be complex.
