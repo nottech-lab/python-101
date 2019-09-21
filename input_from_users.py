adc807ae459352c8ae0f3a185c2352aa14090e7d
daudi mwanja <daudimwanja2014@gmail.com>
2019-09-21 14:28:45 -0700

    input_from_users

--- input_from_users.py
+++ input_from_users.py
@@ -1,3 +1,9 @@
 '''
 Write a simple script that would take input from user using keyboard 
 '''
+# input name from user.
+name = input("enter your name: ")
+# input age from user.
+age = input("enter your age: ")
+# ouput message.
+print("Hello",name + " your ",age)
