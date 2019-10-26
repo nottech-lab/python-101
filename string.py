392fcaaef8fb2bd6444668f84f6178b1d129d126
lebrony <asandler778@gmail.com>
2019-10-27 00:43:21 +0300

    Update string.py

--- string.py
+++ string.py
@@ -1,23 +1,21 @@
-# '''
-# Write a simple script to demonstrate your understanding of strings 
-# '''
-x = "There are %d types of people." % 10
-binary = "binary"
-do_not = "don't"
-y = "Those who knows %s and those who %s "%(binary, do_not)
+'''
+Write a simple script to demonstrate your understanding of strings 
+'''
 
-print x
-print y
-
-print "I said: %r." % x
-print "I also said: '%s" % y
-
-hilarious = "false"
-joke_evaluation = "Isn't that a joke so funny?! %r"
-
-print joke_evaluation % hilarious
-
-w = "This is the left side of "
-e = "a string with a right side"
-
-print w + e
+# Creating a String  
+# with single Quotes 
+String1 = 'Welcome to the Nontech World'
+print("String with the use of Single Quotes: ") 
+print(String1) 
+  
+# Creating a String 
+# with double Quotes 
+String1 = "I'm a Adam"
+print("\nString with the use of Double Quotes: ") 
+print(String1) 
+  
+# Creating a String 
+# with triple Quotes 
+String1 = '''hey i'm adam and I live at "Arusha"'''
+print("\nString with the use of Triple Quotes: ") 
+print(String1) 
