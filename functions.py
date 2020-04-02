0f10bf7a9c45071b8ec88188843ee076ba11d119
Malasaj <59065660+Malasaj@users.noreply.github.com>
2020-04-02 15:55:13 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -2,3 +2,17 @@
 Write a simple script that demonstrate your understanding of functions in python 
 '''
 
+#Function without parameter
+
+def name():
+	print("This is python learnig lab")
+
+# Function with parameter
+
+def add(a,b):
+	total = a+b
+	print("The Sum of these numbers is ", total)
+
+# Call the function
+
+add(5, 10)
