fd37acdd7e4664c390a7552080e5ac69dd454d24
unknown <gevinco96@gmail.com>
2020-04-09 02:34:54 +0300

    implement users inputs

--- input_from_users.py
+++ input_from_users.py
@@ -1,3 +1,11 @@
 '''
 Write a simple script that would take input from user using keyboard 
 '''
+num = input ("Enter number :") 
+print(num) 
+name1 = input("Enter name : ") 
+print(name1) 
+  
+# Printing type of input value 
+print ("type of number", type(num)) 
+print ("type of name", type(name1)
