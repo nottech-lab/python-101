1dd951fc8d128639ec6eae65e35da706ecb19552
unknown <gevinco96@gmail.com>
2020-04-09 02:05:47 +0300

    implement condition

--- conditional_statements.py
+++ conditional_statements.py
@@ -3,3 +3,14 @@ Write a simple script that demonstrate your understanding of conditional stateme
 Simple If , if else , elif  
 '''
 
+if __name__ == '__main__':
+    n = int(input())
+   if (n %2 ==1):
+   print("Weird")
+    else
+    if (n>=2  and n<=5):
+        print("Not Weird")
+    elif(n >= 6 and n<=20):
+        print ("weird")
+        elif(n>20)
+        print("Not weird")
