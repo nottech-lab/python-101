b5688a5d510591062242f07fd8d4936a396eaf27
unknown <gevinco96@gmail.com>
2020-04-10 22:52:56 +0300

    implement dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,3 +1,6 @@
 '''
 Write a simple script to demonstrate your understanding of dictionaries as data types 
 '''
+dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+print ("dict['Name']: "), dict['Name']
+print ("dict['Age']: "), dict['Age']
