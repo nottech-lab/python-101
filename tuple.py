bb98462f49517a7daa51acbaaab1cd4328ad3ef1
unknown <gevinco96@gmail.com>
2020-04-10 23:24:27 +0300

    access value in tuple

--- tuple.py
+++ tuple.py
@@ -1,3 +1,7 @@
 '''
 Write a simple script to demonstrate your understanding of tuples as data types 
 '''
+tup1 = ('physics', 'chemistry', 1997, 2000);
+tup2 = (1, 2, 3, 4, 5, 6, 7 );
+print ("tup1[0]: "), tup1[0];
+print ("tup2[1:5]: "), tup2[1:5];
