b1415c165feeb1280b142194d81906d49f1e2f8d
unknown <gevinco96@gmail.com>
2020-04-10 22:31:49 +0300

    implement loop

--- loop.py
+++ loop.py
@@ -1,3 +1,8 @@
 '''
 Write a simple script that demonstrate your understanding of loops in python   
 '''
+count = 0
+while (count < 9):
+   print ('The count is:'), count
+   count = count + 1
+print("Good bye!")
