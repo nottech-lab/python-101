408731fc09b38b2accd4cfd426fe0c8edc49bf55
unknown <gevinco96@gmail.com>
2020-04-10 22:24:22 +0300

    implement list understand

--- list.py
+++ list.py
@@ -1,3 +1,8 @@
 '''
 Write a simple script to demonstrate your understanding of lists 
 '''
+
+list1 = ['physics', 'chemistry', 1997, 2000];
+list2 = [1, 2, 3, 4, 5, 6, 7 ];
+print "list1[0]: ", list1[0]
+print "list2[1:5]: ", list2[1:5]
