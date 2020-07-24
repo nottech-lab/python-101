837319aa582994f8daea7900f7d454c000968ebb
Stephen_c1 <65715688+Stephensdev@users.noreply.github.com>
2020-07-24 04:40:39 +0300

    Update variables.py
    my first variable task

--- variables.py
+++ variables.py
@@ -1,3 +1,23 @@
 '''
 Write a simple script to demonstrate your understanding of Variable,identifier and keywords
 '''
+#assigning a variable
+
+lesson='pythonlesson'
+
+print(lesson)
+
+#assigning multiple variables
+
+lesson,session,year ='python','batch5','2020'
+
+print(lesson)
+print(session)
+print(year)
+
+
+color='red'
+hobbie='coding'
+
+print(color)
+print(hobbie)
