70bfd86c4e45ce6a3f3a7d9d36b01f61c1c29177
Phinamihayo <JosephineMihayo@gmail.co>
2020-07-27 15:11:26 +0300

    added few string variable

--- string.py
+++ string.py
@@ -1,3 +1,10 @@
-'''
-Write a simple script to demonstrate your understanding of strings 
-'''
+my_string="Python for beginners"
+print (my_string)
+print(my_string[3]) 
+print(len(my_string))
+print (my_string.upper())
+print (my_string.lower())
+
+print(my_string.find("beginners"))
+print(my_string.replace("beginners","absolute beginners"))
+print(my_string.find("y"))
