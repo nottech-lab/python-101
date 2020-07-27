ea86cd1d02247f0d81445a6ea5efe9de83731d23
Phinamihayo <JosephineMihayo@gmail.co>
2020-07-27 14:37:31 +0300

    added few variables and i initialized them

--- variables.py
+++ variables.py
@@ -1,3 +1,24 @@
 '''
 Write a simple script to demonstrate your understanding of Variable,identifier and keywords
 '''
+
+
+#Write code exampled with comments that demonstrates your understanding of variables. Your code should be well stru
+#ctured and variable naming should be epic!
+name = "John"
+age = 35
+gender = "male"
+print(name)
+print(age)
+print(gender)
+
+
+
+president = "Kikwete"
+country = "Tanzania"
+number_people = 104
+freindly_country = "Kenya","Uganda"
+print(president)
+print(country)
+print(number_people)
+print(freindly_country)
