2546c0012d6b33e5145824e4d076776590f358f0
Phinamihayo <JosephineMihayo@gmail.co>
2020-07-28 19:22:48 +0300

    added a few numbers

--- variables.py
+++ variables.py
@@ -1,3 +1,30 @@
 '''
 Write a simple script to demonstrate your understanding of Variable,identifier and keywords
 '''
+#Int
+a = 1
+b = 34555
+c = -453
+print(type(a))
+print(type(b))
+print(type(c))
+
+x = 5
+y = 3.9
+z = 8j
+print(type(x))
+print(type(y))
+print(type(z))
+
+#float
+x = 5.10
+y = 1.5
+z = -22.34
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+
+
+
