890c026a9cdbe64e3d7dd84ed85cabab9cd32d27
Phinamihayo <JosephineMihayo@gmail.co>
2020-07-28 13:20:59 +0300

    added a few operators tasks

--- operators.py
+++ operators.py
@@ -1,4 +1,44 @@
 '''
 Write a simple script that demonstrate your understanding of Operators,operands and operator precedence
 '''
+#Addition
+x = 6
+y = 7
+print(x+y)
+
+#Multiplication
+x = 12
+x *=4
+print(x)
+
+#Division
+a = 102
+b = 8
+print(a/b)
+
+#Substraction
+a = 1042
+b = 65
+print(a-b)
+
+#Modulus
+a = 65
+b = 3
+print(a%b)
+
+#comparison operator
+x = 7
+b = 2
+print(x<b)
+
+x = 8
+y = 4
+print(x>y)
+
+
+
+
+
+
+
 
