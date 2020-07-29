730ed0ae2e6e80caac639d2234404b9a2ee120b6
Phinamihayo <JosephineMihayo@gmail.co>
2020-07-29 17:17:59 +0300

    added example of module

--- functions.py
+++ functions.py
@@ -1,4 +1,26 @@
 '''
 Write a simple script that demonstrate your understanding of functions in python 
 '''
+#create a module
+person1 = {
+    "name": "Josephine",
+    "year": 2002,
+    "age":  48,
+    "country": "Tanzania",
+    "weight":  87,
+    "address": "Arusha",
+    "gender":  "Female"
+    }
+print(person1)
+print("name")
+print("year")
+print("age")
+print("country")
+print("weight")
+print("address")
+print("gende")
+
+import mymodule
+a = mymodule.person1["age"]
+print(a)
 
