ab809b2621943a2f3f47e42602f25f105111c9f4
Phinamihayo <JosephineMihayo@gmail.co>
2020-07-29 16:14:15 +0300

    added a few function task

--- functions.py
+++ functions.py
@@ -1,4 +1,18 @@
 '''
 Write a simple script that demonstrate your understanding of functions in python 
-'''
+''' 
+def printHello(name):  ### PRINT
+    print(name)
+def getArea(length, width): ## INPUT
+    area = length * width
+    return area
+
+def ola(name, age, sex):
+    print("Hello", name, "Your age is", age, "your sex is", sex)
+ola("Neema Daudi", 40, "F")
+ola("Erasto Ayoub", 3, "M")
+printHello("Monica")
+
+
+
 
