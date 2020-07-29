adb67ec0150dc88380ade312887aae8b2c4c6e0c
Stephen_c1 <65715688+Stephensdev@users.noreply.github.com>
2020-07-30 00:13:40 +0300

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,3 +1,27 @@
 '''
 Write a simple script that demonstrate your understanding of loops in python   
 '''
+
+#count loop
+
+count = 1
+while (count < 6):
+   print 'The count is:', count
+   count = count + 1
+
+print "Count!"
+
+
+#break loop
+
+for letter in 'Python': 
+   if letter == 'h':
+      break
+   print 'Current Letter :', letter
+
+
+#Sequence
+
+    number = ["1","2","3","4"]
+    for n in number:
+        print(n)
