84e6c39ec4f34f52f804742d9f6bd1a9f3cfe7ce
Stephen_c1 <65715688+Stephensdev@users.noreply.github.com>
2020-07-29 06:17:55 +0300

    Update conditional_statements.py

--- conditional_statements.py
+++ conditional_statements.py
@@ -3,3 +3,34 @@ Write a simple script that demonstrate your understanding of conditional stateme
 Simple If , if else , elif  
 '''
 
+#if script statement
+
+Apples = 10
+if ( Apples == 10 ) : print "There are 10 Apples"
+print "Wellcome!"
+
+#if else script statement
+
+stephen = 24
+charles = 61
+
+if stephen > charles:
+
+    print("Stephen is Older")
+
+else:
+
+    print ("charles is Older")
+
+#elif script statement
+
+x = 10
+y = 20
+z = 30
+ 
+if x>y:
+    print("x is greater than y")
+elif x==z:
+    print("x is less than z")    
+else:
+    print("if and elif(s) never ran")
