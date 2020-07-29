4500d732b255fb5770c766114cbc7830cc20379b
Stephen_c1 <65715688+Stephensdev@users.noreply.github.com>
2020-07-30 02:20:09 +0300

    Update string.py

--- string.py
+++ string.py
@@ -1,3 +1,29 @@
 '''
 Write a simple script to demonstrate your understanding of strings 
 '''
+#string
+
+string1 = "python"
+
+string2 = "hen"
+
+print(string1,string2)
+
+#string update/ replace
+
+val = 'Hello World!'
+
+print "Updated String :- ", val[:6] + 'welcome to Python!'
+
+#capitalize
+
+cap = "python"
+
+print (cap.upper())
+
+
+#finding
+
+classes ="coding lesson"
+
+print(classes.find("s"))
