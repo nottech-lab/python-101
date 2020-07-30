23750338b7857997b6284df37ceaafbe2eb5e0cb
Stephen_c1 <65715688+Stephensdev@users.noreply.github.com>
2020-07-30 23:16:45 +0300

    Update tuple.py

--- tuple.py
+++ tuple.py
@@ -1,3 +1,13 @@
 '''
 Write a simple script to demonstrate your understanding of tuples as data types 
 '''
+
+#accessing values in tuples
+
+subjects = ('physics', 'chemistry', 2020, 2025);
+number = (10, 27, 39, 46, 75, 66, 73 );
+add = subjects + number
+
+print "subjects[0]: ", subjects[0];
+print "number[1:5]: ", number[1:5];
+print add
