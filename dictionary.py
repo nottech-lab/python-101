fde0ada3d85daa7543c74e4a3bc2b7dfb9383764
Stephen_c1 <65715688+Stephensdev@users.noreply.github.com>
2020-07-31 00:47:22 +0300

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,3 +1,10 @@
 '''
 Write a simple script to demonstrate your understanding of dictionaries as data types 
 '''
+
+
+dictionary = {'Name': 'Rodrick', 'Age': 57, 'Class': 'First'}
+
+print "dictionary['Name']: ", dictionary['Name']
+print "dictionary['Age']: ", dictionary['Age']
+print "dictionary['Class']: ", dictionary['Class']
