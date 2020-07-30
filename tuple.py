2f4e520229546977866d70438bfeac7409f0f453
Stephen_c1 <65715688+Stephensdev@users.noreply.github.com>
2020-07-30 23:21:22 +0300

    Update tuple.py

--- tuple.py
+++ tuple.py
@@ -8,6 +8,6 @@ subjects = ('physics', 'chemistry', 2020, 2025);
 number = (10, 27, 39, 46, 75, 66, 73 );
 add = subjects + number
 
-print "subjects[0]: ", subjects[0];
-print "number[1:5]: ", number[1:5];
-print add
+print ("subjects[0]: ", subjects[0])
+print ("number[1:5]: ", number[1:5])
+print (add)
