8c19c8d7032a0d9e1e89a60c28a5c7ef12bdd4ab
mchomvus4 <samwelnm4@gmail.com>
2020-07-31 14:30:30 +0300

    added a few condtional statement

--- conditional_statements.py
+++ conditional_statements.py
@@ -3,3 +3,26 @@ Write a simple script that demonstrate your understanding of conditional stateme
 Simple If , if else , elif  
 '''
 
+if 5 > 2:
+    print("Five is greater than two")
+#end of if statement
+
+
+a = 10
+b = 20
+if a > b:
+    print ("The value of a is greater than b")
+else:
+    print ("The value of b is greater than a")
+    
+#end of if else statement
+
+gross = 2000
+basic = 15000
+otherIncome = 15000
+
+if gross > basic:
+    print ('My gross is greater than basic')
+elif basic == otherIncome:
+    print('Other income is the same as basic')
+
