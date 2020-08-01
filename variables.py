1d815d0eae0b96101cf29791f3da85a57c148df7
mchomvus4 <samwelnm4@gmail.com>
2020-08-01 18:19:06 +0300

    added few variable, identifier and keyword

--- variables.py
+++ variables.py
@@ -1,3 +1,13 @@
 '''
 Write a simple script to demonstrate your understanding of Variable,identifier and keywords
 '''
+
+x = 50
+print (x)
+
+myName = "Samwel Mchomvu"
+print (myName)
+
+doubleBalance  #identifier
+
+# int  await  def  keywords
