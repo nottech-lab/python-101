0c7990279822bb5a5d00238fa2763ee5686d7724
Stephen_c1 <65715688+Stephensdev@users.noreply.github.com>
2020-08-03 01:38:53 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -2,3 +2,12 @@
 Write a simple script that demonstrate your understanding of functions in python 
 '''
 
+#functions
+
+def me():
+    name, age = "Stephen", 24
+    hobbie= "Coding"
+    address = "Dodoma, Tanzania"
+    print("Name: {}\nAge: {}\nHobbie {}\nAddress: : {}".format(name, age,hobbie, address))
+
+me()
