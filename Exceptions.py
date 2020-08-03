ba036667e6c57b8c59c2308139fdaa119e3bba5e
Stephen_c1 <65715688+Stephensdev@users.noreply.github.com>
2020-08-03 03:46:18 +0300

    Create Exceptions.py

---
+++ Exceptions.py
@@ -0,0 +1,13 @@
+'''
+Write a simple script to demonstrate your understanding of Files Exceptions
+
+ 
+'''
+try:
+   fh = open("testfile", "w")
+   fh.write("This is my test file for exception handling!!")
+except IOError:
+   print "Error: can\'t find file or read data"
+else:
+   print "Written content in the file successfully"
+   fh.close()
