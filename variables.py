bb3680c894fc9d23d47a496153e70560b4cf0171
reggygshayo <reggygshayo@gmail.com>
2020-08-04 10:54:52 +0300

    add some variable

--- variables.py
+++ variables.py
@@ -1,3 +1,14 @@
 '''
 Write a simple script to demonstrate your understanding of Variable,identifier and keywords
 '''
+#variable for dictionary
+dict = {}
+dict['py'] = "Python test"
+dict[7]     = "Test in Github"
+
+sale = {'Basket': 'Ball'}
+
+
+print dict['py']      
+print dict[7]           
+print sale
