58ee3c5f305374171b78791fcc7ff5867355a170
silvar1122 <59538986+silvar1122@users.noreply.github.com>
2020-08-19 12:00:21 -0700

    operators,operands and precedence

--- operators.py
+++ operators.py
@@ -2,3 +2,33 @@
 Write a simple script that demonstrate your understanding of Operators,operands and operator precedence
 '''
 
+#operations
+x=5
+y=2
+#adding and displaying the output
+z=x+y
+print(x)
+
+#subtracting and displaying the output
+a=x-y
+print(a)
+
+#multiplying and displaying the output
+b=x*y
+print(b)
+
+#dividing and displaying the output
+c=x/y
+print(c) #answer will be a float
+d=x//y
+print(d) #answer will be an integer
+
+#raising to a power and displaying the output
+e=x**y
+print(e)
+
+
+#operator predecence
+#calculations are done in the order of bracket opening,exponents,multiplication or division,addition or substraction
+x=(3*15/5)+2**2-1
+print(x)
