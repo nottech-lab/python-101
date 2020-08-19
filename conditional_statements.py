ff81c3efc4a4882b3517d77ad3c50351b6fd9ff3
silvar1122 <59538986+silvar1122@users.noreply.github.com>
2020-08-19 11:35:37 -0700

    Using if, if else and elif

--- conditional_statements.py
+++ conditional_statements.py
@@ -2,4 +2,28 @@
 Write a simple script that demonstrate your understanding of conditional statements 
 Simple If , if else , elif  
 '''
+#using if statement
+hot=True
+if hot:
+  print('Wear light clothes')
+print('Its a wonderful day')
 
+
+#using if else statement
+is_hot_day=True
+if is_hot_day:
+    print('Drink alot of water')
+else:
+    print('Wear a sweater')
+print("it's a wonderfull day")
+
+
+#using elif
+is_hot=False
+is_cold=True
+if is_hot:
+    print('Wear light clothes')
+elif is_cold:
+    print('Wear warm clothes')
+else:
+    print('Its a wonderfull day')
