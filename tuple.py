a5f5dda77991b70b46f153761dca2e698308b868
silvar1122 <59538986+silvar1122@users.noreply.github.com>
2020-08-22 22:42:03 -0700

    Using topples
    Topple are just like lists but these use parenthesis and are permanent( Nothing can be added nor removed from them)

--- tuple.py
+++ tuple.py
@@ -1,3 +1,18 @@
 '''
 Write a simple script to demonstrate your understanding of tuples as data types 
 '''
+# Topples
+
+given_numbers=(1,2,3)
+# acces is obtained just like lists
+print(given_numbers[1])
+
+# assigning items of a kist to variables
+ x=given_numbers[0]
+ y=given_numbers[1]
+ z=given_numbers[2]
+  
+# altenatively by unpacking
+a,b,c=given_numbers
+print(a)
+
