78cd596d4e0975b6d77d37297c919b8db4f6547c
silvar1122 <59538986+silvar1122@users.noreply.github.com>
2020-08-22 22:35:39 -0700

    using loops
    Loops enable programmer to iterate through a list. Or repeat a number of tasks at a given interval

--- loop.py
+++ loop.py
@@ -1,3 +1,14 @@
 '''
 Write a simple script that demonstrate your understanding of loops in python   
 '''
+#using while loop
+counter=0
+counter_limit=3
+while counter<counter_limit:
+    print("Welcome")
+    counter=counter+1
+
+#Using for loop
+#loop iterates through a list
+for names in ['Elizabeth','Gilbert','Filbert']:
+    print(f'welcome {names}')
