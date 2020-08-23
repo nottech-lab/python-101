e6bdcff13cc071180d1458d316de1a8bdef2d694
silvar1122 <elizasenkoro@gmail.com>
2020-08-23 07:28:40 -0700

    Dictionaries carry attributes of a particular thing

---
+++ Senkoro_dictionary.py
@@ -0,0 +1,14 @@
+# Stores attributes of a thing(characteristics of a thing
+
+
+Elizabeth={
+    'age':10,
+    'school':'Mazinde Juu',
+    'is old enought':'True'
+}
+# Accesed by square bracket method
+print(Elizabeth['school'])
+print(Elizabeth.get('age')) # with get method if you enter the wrong key it wont lead to error
+# adding to the dictionary
+Elizabeth['Birthdate']='1/11/2020'
+print(Elizabeth['Birthdate'])
