4b68979f222fcd76fae08d878de803b92b85134b
silvar1122 <elizasenkoro@gmail.com>
2020-08-28 12:55:37 -0700

    functions

---
+++ Senkoro_function2.py
@@ -0,0 +1,9 @@
+#function
+def summation(number1,number2):
+    sum=number1+number2
+    return sum
+
+
+input1=int(input('number1:'))
+input2=int(input('number2:'))
+print('summatiom=',summation(input1,input2))
