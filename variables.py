1954447bee0e2ec8ed57fea184aea63455ff510c
Bryan <73223404+Bryan-JK@users.noreply.github.com>
2020-11-13 12:34:16 +0300

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,3 +1,34 @@
 '''
 Write a simple script to demonstrate your understanding of Variable,identifier and keywords
 '''
+
+#examples of values assigned to variables 
+name =' universe '
+point = ' a friendly'
+print(point+ name)
+
+#variables outside a function 
+
+l = 'life'
+def myfunc():
+    l = 'God'
+    print( l + 'is good')
+myfunc()
+
+print( l + 'is good')
+
+# using the global keyword the variable inside the function becomes dominant
+
+l = 'life'
+def my_func():
+    global l
+    l = 'God '
+    print( l + 'is good')
+myfunc()
+
+print( l + 'is good')
+
+#an simple example of an identifier 
+
+my_class1 = 'python 101'
+print(my_class1)
